dfdedb096cab3c02818bd1098c01196816cd0423
PedroAugustoMD <pedro.augustohp02@gmail.com>
2025-08-19 10:22:31 -0300

    Atividade pratica 01 - Questao 03

---
+++ atividadepratica01/questao03.py
@@ -0,0 +1,14 @@
+'''
+* Comprimento: 12 cm
+* Largura: 14 cm
+* Altura: 20 cm
+'''
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print(f"O volume da caixa é de {volume} cm³")
+print("O volume da caixa é de", volume, "cm³")
