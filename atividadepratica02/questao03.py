80b5f733d87596d2c00ac3fc06727de19e9933bb
PedroAugustoMD <pedro.augustohp02@gmail.com>
2025-08-21 10:09:12 -0300

    Atividade pratica 02 - Questao 03

---
+++ atividadepratica02/questao03.py
@@ -0,0 +1,18 @@
+"""
+* Nota 1: 7.5
+* Nota 2: 8.0
+* Nota 3: 6.5
+
+"""
+
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("Notas do aluno: ")
+print("Nota 1:", nota1)
+print("Nota 2:", nota2)
+print("Nota 3:", nota3)
+print("Média final:", round(media, 2))
